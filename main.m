c4ad4b18fe83ea66d485d0b47aa3ba1674917653
IngoMarquart on RS2 <ingo.marquart@esmt.org>
2019-04-02 19:55:59 +0200

    state-space / archetypes fix

--- main.m
+++ main.m
@@ -16,22 +16,34 @@ paramsDefault.mn=0; % M parameter for G network (Jackson&Rogers 2014 algorithm)
 
 
 %% List of parameters to run
-nList=[50:80];
+% State Space
+nList=[50:5:80];
 mList=[1:30];
 eList=[0.10,  0.30,  0.50,  0.70, 0.90, 1.00, 5.00, 50,100,500];
 consList=[-1,-0.5,0,0.5,1];
 
+% Archetypes
+nList=[15:5:70];
+mList=[1:30];
+eList=[0.10,  0.30,  0.50,  0.70, 0.90, 1.00, 5.00, 50,100,500];
+consList=[-1,-0.5,0,0.5,1];
 %% Type settings
 % Probabilities of climbers relative to slackers. 
 % Simulation will check symmetrically for slackers
 % Archetypes
-%PCscale=0.25:0.25:0.5;
+PCscale=0.25:0.25:0.5;
 % State Space
-PCscale=0.15:0.05:0.5;
+% PCscale=0.15:0.05:0.5;
 % Overall probability of watchers. Can also be a vector to check for several levels of watchers
-Wscale=[2/9,1/3,2/3];
+% Archetypes
+Wscale=[2/9,2/3];
+% State Space
+% Wscale=[2/9,1/3,2/3];
 %% Theta settings
+% Archetypes
 thetascale=[2:0.5:7];
+% State Space
+% thetascale=[2:0.5:7];
 
 %% Create a cell array of parameters to run
 paramsCell=createParamCell(PCscale,Wscale,thetascale,mList,nList,eList,consList,paramsDefault);
