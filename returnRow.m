863776eb918ec209cf6ad3d7088996eb73dc5ef6
IngoMarquart <ingo.marquart@esmt.org>
2019-05-23 14:49:23 +0200

    Added Conutil to table

--- returnRow.m
+++ returnRow.m
@@ -5,28 +5,30 @@
 % @param: firm- a firm
 % @return: row - a 1-row table
 %%
-function row = returnRow(firm,T)
-firmID=string(firm.firmID);
-n=firm.n;
-e=firm.eMat(T);
-m=firm.m;
-diffM=firm.diffM(T);
-SM=firm.SM(T);
-expSM=firm.expSM(T);
-avgX=firm.avgX(T);
-avgTheta=firm.avgTheta(T);
-varSM=firm.varSM(T);
-rationality=firm.rationality;
-maxDiff=firm.maxDiff(T);
-minDiff=firm.minDiff(T);
-thetaRange=firm.thetaRange;
-realSkew=firm.skew(T);
-realCons=firm.cons(T);
-paramCons=firm.startCons;
-NrC=firm.NrC;
-NrW=firm.NrW;
-NrS=firm.NrS;
-Talpha=firm.Talpha;
-Tbeta=firm.Tbeta;
-row=table(firmID,T,n,m,e,rationality,NrC,NrW,NrS,Talpha,Tbeta,realSkew,realCons,paramCons,avgX,avgTheta,expSM,SM,varSM,diffM,maxDiff,minDiff,thetaRange);
+function row = returnRow(firm, T)
+    firmID = string(firm.firmID);
+    n = firm.n;
+    e = firm.eMat(T);
+    m = firm.m;
+    diffM = firm.diffM(T);
+    SM = firm.SM(T);
+    expSM = firm.expSM(T);
+    avgX = firm.avgX(T);
+    avgTheta = firm.avgTheta(T);
+    varSM = firm.varSM(T);
+    rationality = firm.rationality;
+    maxDiff = firm.maxDiff(T);
+    minDiff = firm.minDiff(T);
+    thetaRange = firm.thetaRange;
+    realSkew = firm.skew(T);
+    realCons = firm.cons(T);
+    paramCons = firm.startCons;
+    NrC = firm.NrC;
+    NrW = firm.NrW;
+    NrS = firm.NrS;
+    Talpha = firm.Talpha;
+    Tbeta = firm.Tbeta;
+    rationality = firm.rationality;
+    conUtil = firm.conUtil;
+    row = table(firmID, T, n, m, e, rationality, NrC, NrW, NrS, Talpha, Tbeta, realSkew, realCons, paramCons, avgX, avgTheta, expSM, SM, varSM, diffM, maxDiff, minDiff, thetaRange, rationality, conUtil);
 end
